829324725b09f94b8d9777d76f8d9e85544f82b7
squarefaceYT <65513144+squarefaceYT@users.noreply.github.com>
2020-05-18 01:12:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const cfg = require('./index.json');
+const prefix = ("/");
+
+bot.on('ready', function () {
+    console.log("Je suis prêt à être utilisé. !")
+    bot.user.setActivity('être le meilleur des bots.').catch(console.error)
+  })
+
+
+bot.on('message', message => {
+    if (message.content === '/prends') {
+      message.reply('a pris votre commande, nous faisons le nécessaire afin que vous soyez livré au plus vite, vous serez ping pour la réception de votre commande , Merci de votre patience') 
+     
+      message.delete()
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      .catch(console.error);
+
+
+    }
+  })
+
+  bot.on('message', message => {
+    if (message.content === '/pret') {
+      message.reply('*La transaction sera sur le serveur de votyre choix, la livraison sera de la manière suivante: dans un premier temps vous payé le vendeur et il se tp à vous ou inverse et donne la commande*') 
+     
+      message.delete()
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      .catch(console.error);
+
+
+    }
+  })
+
+  bot.on('message', message => {
+    if (message.content === '/fini') {
+      message.reply(' Nous vous remercions pour votre achat et à bientôt sur le PurpleMarket Je vous invite a écrire la commande /ticket close dans ce Channel ! Vous pouvez aussi nous laisser un avis dans #『⭐』𝐀𝐯𝐢𝐬') 
+
+      message.delete()
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      .catch(console.error);
+    
+    
+    }
+})
+      
+
+  bot.login(process.env.TOKEN);
+  
